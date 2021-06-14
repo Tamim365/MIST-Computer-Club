9a1a866e51358d3cf8052247d89d5f839e65383e
Shiary29 <shiaryk29@gmail.com>
2021-06-15 01:31:31 +0600

    member profile page updated

--- resources/views/member/profile.blade.php
+++ resources/views/member/profile.blade.php
@@ -54,7 +54,7 @@ body{
 .card-body {
     flex: 1 1 auto;
     min-height: 1px;
-    padding: 1rem;
+    padding: 3rem;
 }
 
 .gutters-sm {
@@ -88,10 +88,12 @@ body{
   margin-top: 1rem;
 }
 p {
-  text-align: center;
-  max-width: 600px;
-  padding: 1em;
-  background: #eee;
+ text-align: center;
+ color: rgb(22, 13, 139);
+  font-size: large;
+  max-width: 700px;
+  padding: 0.5em;
+  background: rgb(255, 255, 255);
 }
 h1 {
   text-align: center;
@@ -143,42 +145,51 @@ h1 {
                 </div>
                 <div class="card mt-3">
                     <ul class="list-group list-group-flush">
+
                         <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                             <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round"
-                                    class="feather feather-globe mr-2 icon-inline">
-                                    <circle cx="12" cy="12" r="10"></circle>
-                                    <line x1="2" y1="12" x2="22" y2="12"></line>
+                                    {{-- class="feather feather-globe mr-2 icon-inline"> --}}
+                                    {{-- <circle cx="12" cy="12" r="10"></circle> --}}
+                                    {{-- <line x1="2" y1="12" x2="22" y2="12"></line> --}}
                                     <path
                                         d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z">
                                     </path>
-                                </svg>Website</h6>
-                            <span class="text-secondary">https://siri.com</span>
+                                </svg>Panel Role</h6>
+                            <span class="text-secondary">SINGLE</span>
                         </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+
+
+                        {{-- <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                             <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round"
-                                    class="feather feather-github mr-2 icon-inline">
+                                     class="feather feather-github mr-2 icon-inline"> 
+
                                     <path
                                         d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
                                     </path>
-                                </svg>Github</h6>
-                            <span class="text-secondary">siri</span>
-                        </li>
+                                </svg>Committee_Name</h6>
+                            <span class="text-secondary">dummy</span>
+                        </li> --}}
+
+
                         <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                             <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round"
-                                    class="feather feather-twitter mr-2 icon-inline text-info">
+                                    {{-- class="feather feather-twitter mr-2 icon-inline text-info"> --}}
+
                                     <path
                                         d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z">
                                     </path>
-                                </svg>Twitter</h6>
-                            <span class="text-secondary">@siri</span>
+                                </svg>Committee_Name</h6>
+                            <span class="text-secondary">TIT</span>
                         </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+
+
+                        {{-- <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                             <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                     stroke-linecap="round" stroke-linejoin="round"
@@ -197,7 +208,7 @@ h1 {
                                     <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                                 </svg>Facebook</h6>
                             <span class="text-secondary">shiary</span>
-                        </li>
+                        </li> --}}
                     </ul>
                 </div>
             </div>
@@ -205,7 +216,9 @@ h1 {
                 <div class="card mb-3">
                     <div class="card-body">
                         <div class="row">
+                            <p>GENERAL INFORMATION</p>
                             <div class="col-sm-3">
+
                                 <h6 class="mb-0">Full Name</h6>
                             </div>
                             <!-- <div class="col-sm-9 text-secondary">
@@ -216,18 +229,9 @@ h1 {
                             </div>
                         </div>
                         <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">Email</h6>
-                            </div>
-                            <!-- <div class="col-sm-9 text-secondary">
-                                siri@gamil.com
-                            </div> -->
-                            <div class="col-sm-9 text-secondary">
-                                <input type="text" class="form-control" value="{{$email}}" name = "email">
-                            </div>
-                        </div>
-                        <hr>
+
+
+
                         <div class="row">
                             <div class="col-sm-3">
                                 <h6 class="mb-0">Student ID</h6>
@@ -239,7 +243,8 @@ h1 {
                                 <input type="text" class="form-control" value="{{$st_id}}" name = "student_id">
                             </div>
                         </div>
-                        <hr>
+                        <br>
+                       
 
                         <hr>
                         <div class="row mb-3">
@@ -250,15 +255,20 @@ h1 {
                                  <input type="text" class="form-control" value="" name = "address"> 
                             </div>
                         </div>
+                        
                         <hr>
                         <div class="row mb-3">
                             <div class="col-sm-3">
-                                <h6 class="mb-0">Age</h6>
+                                <h6 class="mb-0">Date of Birth</h6>
                             </div>
                             <div class="col-sm-9 text-secondary">
-                                <input type="text" class="form-control" value="" name = "age">
+                                <input type="text" class="form-control" name = "dob" data-date-format="dd-mm-yyyy" value="15-October-2000" id="dp2">
                             </div>
                         </div>
+
+
+
+
                         <hr>
                         <div class="row mb-3">
                             <div class="col-sm-3">
@@ -305,7 +315,26 @@ h1 {
                                 <input type="text" class="form-control" value="" name = "level">
                             </div>
                         </div>
+                      
+
+                        <p>ACCOUNT INFORMATION</p>
+
+                        
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <h6 class="mb-0">Email</h6>
+                            </div>
+                            <!-- <div class="col-sm-9 text-secondary">
+                                siri@gamil.com
+                            </div> -->
+                            <div class="col-sm-9 text-secondary">
+                                <input type="text" class="form-control" value="{{$email}}" name = "email">
+                            </div>
+                        </div>
                         <hr>
+
+
+
                         
                         <div class="row mb-3">
                             <div class="col-sm-3">
@@ -315,22 +344,14 @@ h1 {
                                 <input type="text" class="form-control" value="" name = "phone">
                             </div>
                         </div>
+                        
                         <hr>
                         <div class="row mb-3">
                             <div class="col-sm-3">
-                                <h6 class="mb-0">Date of Birth</h6>
+                                <h6 class="mb-0">Club ID</h6>
                             </div>
                             <div class="col-sm-9 text-secondary">
-                                <input type="text" class="form-control" name = "dob" data-date-format="dd-mm-yyyy" value="15-October-2000" id="dp2">
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row mb-3">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">Panel Role</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                <input type="text" class="form-control" value="" name = "panel_role">
+                                <input type="text" class="form-control" value="" name = "club_id">
                             </div>
                         </div>
                         <hr>
@@ -342,15 +363,7 @@ h1 {
                                 <input type="text" class="form-control" value="" name = "assigned_work">
                             </div>
                         </div>
-                        <hr>
-                        <div class="row mb-3">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">Committee_Name</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                <input type="text" class="form-control" value="" name = "comittee_name">
-                            </div>
-                        </div>
+                       
                         <hr>
                         <div class="row mb-3">
                             <div class="col-sm-3">
