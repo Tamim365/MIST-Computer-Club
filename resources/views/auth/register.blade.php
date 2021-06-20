e533a610024ce63984bd71dba258b2537f56cc79
Shiary29 <shiaryk29@gmail.com>
2021-06-20 11:28:06 +0600

    2 new registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,36 +13,38 @@
         <div class="row">
             <div class="col-md-3 register-left">
                 <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
-                <h3>Welcome</h3>
+                <h3>Welcome To MCC</h3>
                 <p>You are 30 seconds away from earning your own money!</p>
                 <form action="login.html"><input type="submit" name="" value="Login"></form><br/>
+                {{-- <div class="text-center">Already have an account? <a href="{{route('auth.login')}}">Log in</a></div> --}}
             </div>
             <div class="col-md-9 register-right">
                 <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Employee</a>
+                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Student</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Hirer</a>
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Faculty</a>
                     </li>
                 </ul>
                 <div class="tab-content" id="myTabContent">
                     <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                        <h3 class="register-heading">Apply as a Employee</h3>
+                        <h3 class="register-heading">Register As A Student</h3>
                         <div class="row register-form">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="First Name *" value="" />
+                                    <input type="name" class="form-control" name="name" placeholder="Name" value="{{old('name')}}">
+                                    <span class="text-danger">@error('name'){{ $message }} @enderror</span>
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Last Name *" value="" />
+                                    <input type="Student ID" class="form-control" name="Student_ID" placeholder="Student ID" value="{{old('Student_ID')}}">
+                                    <span class="text-danger">@error('Student_ID'){{ $message }} @enderror</span>
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" class="form-control" placeholder="Password *" value="" />
+                                    <input type="email" class="form-control" name="email" placeholder="Email" value="{{old('email')}}">
+                                    <span class="text-danger">@error('email'){{ $message }} @enderror</span>
                                 </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control"  placeholder="Confirm Password *" value="" />
-                                </div>
+                                
                                 <div class="form-group">
                                     <div class="maxl">
                                         <label class="radio inline"> 
@@ -55,66 +57,76 @@
                                         </label>
                                     </div>
                                 </div>
+                                
                             </div>
+
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="email" class="form-control" placeholder="Your Email *" value="" />
+                                    <input type="password" class="form-control" name="password" placeholder="Password">
+                                    <span class="text-danger">@error('password'){{ $message }} @enderror</span>
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" minlength="10" maxlength="10" name="txtEmpPhone" class="form-control" placeholder="Your Phone *" value="" />
+                                     <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password">
+                                     <span class="text-danger">@error('confirm_password'){{ $message }} @enderror</span>
                                 </div>
-                                <div class="form-group">
+                                {{-- <div class="form-group">
                                     <select class="form-control">
                                         <option class="hidden"  selected disabled>Please select your Sequrity Question</option>
                                         <option>What is your Birthdate?</option>
                                         <option>What is Your old Phone Number</option>
                                         <option>What is your Pet Name?</option>
                                     </select>
-                                </div>
-                                <div class="form-group">
+                                </div> --}}
+                                {{-- <div class="form-group">
                                     <input type="text" class="form-control" placeholder="Enter Your Answer *" value="" />
-                                </div>
+                                </div> --}}
                                 <input type="submit" class="btnRegister"  value="Register"/>
                             </div>
                         </div>
                     </div>
                     <div class="tab-pane fade show" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                        <h3  class="register-heading">Apply as a Hirer</h3>
+                        <h3  class="register-heading">Register As A Faculty</h3>
                         <div class="row register-form">
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="First Name *" value="" />
+                                    <input type="name" class="form-control" name="name" placeholder="Name" value="{{old('name')}}">
+                                    <span class="text-danger">@error('name'){{ $message }} @enderror</span>
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Last Name *" value="" />
+                                    <input type="Student ID" class="form-control" name="Student_ID" placeholder="Student ID" value="{{old('Student_ID')}}">
+                                    <span class="text-danger">@error('Student_ID'){{ $message }} @enderror</span>
                                 </div>
                                 <div class="form-group">
-                                    <input type="email" class="form-control" placeholder="Email *" value="" />
+                                    <input type="email" class="form-control" name="email" placeholder="Email" value="{{old('email')}}">
+                                    <span class="text-danger">@error('email'){{ $message }} @enderror</span>
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" maxlength="10" minlength="10" class="form-control" placeholder="Phone *" value="" />
+                                    <select class="form-control">
+                                        <option class="hidden"  selected disabled>DEPT</option>
+                                        <option>CSE</option>
+                                        <option>EECE</option>
+                                        <option>CE</option>
+                                        <option>ME</option>
+                                        <option>NAME</option>
+                                        <option>NSE</option>
+                                        <option>ARCHI</option>
+                                        <option>IPE</option>
+                                        
+                                    </select>
                                 </div>
 
 
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <input type="password" class="form-control" placeholder="Password *" value="" />
+                                    <input type="password" class="form-control" name="password" placeholder="Password">
+                                    <span class="text-danger">@error('password'){{ $message }} @enderror</span>
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" class="form-control" placeholder="Confirm Password *" value="" />
+                                     <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password">
+                                     <span class="text-danger">@error('confirm_password'){{ $message }} @enderror</span>
                                 </div>
-                                <div class="form-group">
-                                    <select class="form-control">
-                                        <option class="hidden"  selected disabled>Please select your Sequrity Question</option>
-                                        <option>What is your Birthdate?</option>
-                                        <option>What is Your old Phone Number</option>
-                                        <option>What is your Pet Name?</option>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="`Answer *" value="" />
-                                </div>
+                                
                                 <input type="submit" class="btnRegister"  value="Register"/>
                             </div>
                         </div>
